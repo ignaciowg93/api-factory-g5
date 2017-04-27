c9b082e22feea43956ee20fb99281abafb0a26e1
jfcumsille <jfcumsille@uc.cl>
2017-04-26 23:06:07 -0300

    invoice controller complete

--- app/controllers/invoices_controller.rb
+++ app/controllers/invoices_controller.rb
@@ -29,16 +29,49 @@ class InvoicesController < ApplicationController
     
 
     def accepted
+        begin
+            @invoice = set_invoice
+            @invoice.accepted = true
+            render json: {ok: "Factura resuelta recibida exitosamente " }, status: 201
+        rescue ActiveRecord::RecordInvalid 
+            render json:{error: "No se pudo enviar factura resuelta"}, status: 500
+        end
+
         #Change the status of a Invoice in the invoice system.
         #have to validate de id. We generated this Invoice
     end
 
     def rejected
+        begin
+            @invoice = set_invoice
+            @pinvoice.rejected = true
+            render json: {ok: "Factura resuelta recivida exitosamente" }, status: 201
+        rescue ActiveRecord::RecordInvalid 
+            render json:{error: "No se pudo enviar factura resuelta"}, status: 500
+        end
         #Change the status of an Invoice in the system.
     end
 
     def paid
+        begin
+            @invoice = set_invoice
+            @invoice.paid = true
+            render json: {ok: "Factura resuelta recibida exitosamente " }, status: 201
+        rescue ActiveRecord::RecordNotFound 
+            render json:{error: "Id no asociado a OC por pagar"}, status: 404
+        end
         #change the status of an Invoice inthe system. Check for the transaction.
+    end
+
+    def delivered
+        begin
+            @invoice = set_invoice
+            @invoice.delivered = true
+            render json: {ok: "Notificacion recibida exitosamente"}, status:201
+        rescue ActiveRecord::RecordInvalid
+            render json: {error: "No se pudo enviar notificacion"}, status: 500
+        end
+
     end
 
 private
