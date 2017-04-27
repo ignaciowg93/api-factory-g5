2db3b5501473e63420174ca19399cbafbe350eda
ignaciowg93 <ignaciowg93@gmail.com>
2017-04-26 23:35:03 -0300

    Annotate update

--- test/models/invoice_test.rb
+++ test/models/invoice_test.rb
@@ -6,6 +6,12 @@
 #  purchase_order_id :integer
 #  created_at        :datetime         not null
 #  updated_at        :datetime         not null
+#  invoiceid         :string
+#  accepted          :boolean
+#  rejected          :boolean
+#  delivered         :boolean
+#  paid              :boolean
+#  account           :string
 #
 
 require 'test_helper'
