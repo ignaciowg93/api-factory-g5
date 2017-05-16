6db5d6224e932c7c67ab90f01eb4d193370d6926
jfcumsille <jfcumsille@uc.cl>
2017-05-15 22:29:19 -0400

    add cap config for seed

--- config/deploy.rb
+++ config/deploy.rb
@@ -6,6 +6,17 @@ set :repo_url, "git@github.com:ignaciowg93/api-factory-g5.git"
 set :rbenv_path, '/home/deploy/.rbenv'
 set :deploy_to, '/home/deploy/deployapp'
 set :branch, ENV['BRANCH'] if ENV['BRANCH']
-
+# Add this in config/deploy.rb
+# and run 'cap production deploy:seed' to seed your database
+desc 'Runs rake db:seed'
+task :seed => [:set_rails_env] do
+  on primary fetch(:migration_role) do
+    within release_path do
+      with rails_env: fetch(:rails_env) do
+        execute :rake, "db:seed"
+      end
+    end
+  end
+end
 append :linked_files, "config/database.yml", "config/secrets.yml"
 append :linked_dirs, "log", "tmp/pids", "tmp/cache", "tmp/sockets", "vendor/bundle", "public/system", "public/uploads"
