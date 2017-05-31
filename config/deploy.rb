3083bd4f68f15cca336d567989fe8b69c1505493
icrocco <ijcrocco@uc.cl>
2017-05-30 21:21:29 -0400

    porfa deploy con reset 3

--- config/deploy.rb
+++ config/deploy.rb
@@ -14,10 +14,38 @@ task :seed do
     within release_path do
       with rails_env: fetch(:rails_env) do
         #execute :rake, "db:reset DISABLE_DATABASE_ENVIRONMENT_CHECK=1"
+        execute :rake, "db:schema:load"
         execute :rake, "db:seed DISABLE_DATABASE_ENVIRONMENT_CHECK=1"
       end
     end
   end
 end
+
+desc 'Deploy app for first time'
+task :cold do
+  invoke 'deploy:starting'
+  invoke 'deploy:started'
+  invoke 'deploy:updating'
+  invoke 'bundler:install'
+  invoke 'deploy:db_load_schema' # This replaces deploy:migrations
+  invoke 'deploy:compile_assets'
+  invoke 'deploy:normalize_assets'
+  invoke 'deploy:publishing'
+  invoke 'deploy:published'
+  invoke 'deploy:finishing'
+  invoke 'deploy:finished'
+end
+
+desc 'Setup database'
+task :db_load_schema do
+  on roles(:db) do
+    within release_path do
+      with rails_env: (fetch(:rails_env) || fetch(:stage)) do
+        execute :rake, 'db:schema:load'
+      end
+    end
+  end
+end
+
 append :linked_files, "config/database.yml", "config/secrets.yml"
 append :linked_dirs, "log", "tmp/pids", "tmp/cache", "tmp/sockets", "vendor/bundle", "public/system", "public/uploads"
