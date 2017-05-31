75dc444beb3ec427f6ffd15cd5dec1b5d54a9809
icrocco <ijcrocco@uc.cl>
2017-05-30 21:32:41 -0400

    porfa deploy con reset 5

--- config/deploy.rb
+++ config/deploy.rb
@@ -8,45 +8,38 @@ set :deploy_to, '/home/deploy/deployapp'
 set :branch, ENV['BRANCH'] if ENV['BRANCH']
 # Add this in config/deploy.rb
 # and run 'cap production deploy seed' to seed your database
-desc 'Runs rake db:seed'
-task :seed do
-  on primary fetch(:migration_role) do
-    within release_path do
-      with rails_env: fetch(:rails_env) do
-        #execute :rake, "db:reset DISABLE_DATABASE_ENVIRONMENT_CHECK=1"
-        #execute :rake, "db:schema:load"
-        #execute :rake, "db:seed DISABLE_DATABASE_ENVIRONMENT_CHECK=1"
-        execute :rake, "db:setup"
-      end
-    end
-  end
-end
-
-desc 'Deploy app for first time'
+desc "deploy app for the first time (expects pre-created but empty DB)"
 task :cold do
-  invoke 'deploy:starting'
-  invoke 'deploy:started'
-  invoke 'deploy:updating'
-  invoke 'bundler:install'
-  invoke 'deploy:db_load_schema' # This replaces deploy:migrations
-  invoke 'deploy:compile_assets'
-  invoke 'deploy:normalize_assets'
-  invoke 'deploy:publishing'
-  invoke 'deploy:published'
-  invoke 'deploy:finishing'
-  invoke 'deploy:finished'
+  before 'deploy:migrate', 'deploy:initdb'
+  invoke 'deploy'
 end
 
-desc 'Setup database'
-task :db_load_schema do
-  on roles(:db) do
+desc "initialize a brand-new database (db:schema:load, db:seed)"
+task :initdb do
+  on primary :web do |host|
     within release_path do
-      with rails_env: (fetch(:rails_env) || fetch(:stage)) do
+      if test(:psql, 'portal_production -c "SELECT table_name FROM information_schema.tables WHERE table_schema=\'public\' AND table_type=\'BASE TABLE\';"|grep schema_migrations')
+        puts '*** THE PRODUCTION DATABASE IS ALREADY INITIALIZED, YOU IDIOT! ***'
+      else
         execute :rake, 'db:schema:load'
+        execute :rake, 'db:seed'
       end
     end
   end
 end
+# desc 'Runs rake db:seed'
+# task :seed do
+#   on primary fetch(:migration_role) do
+#     within release_path do
+#       with rails_env: fetch(:rails_env) do
+#         #execute :rake, "db:reset DISABLE_DATABASE_ENVIRONMENT_CHECK=1"
+#         #execute :rake, "db:schema:load"
+#         #execute :rake, "db:seed DISABLE_DATABASE_ENVIRONMENT_CHECK=1"
+#         execute :rake, "db:setup"
+#       end
+#     end
+#   end
+# end
 
 append :linked_files, "config/database.yml", "config/secrets.yml"
 append :linked_dirs, "log", "tmp/pids", "tmp/cache", "tmp/sockets", "vendor/bundle", "public/system", "public/uploads"
