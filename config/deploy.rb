3b307c2f2ea8e13707078179aec93dac9a82efcd
icrocco <ijcrocco@uc.cl>
2017-05-30 20:30:16 -0400

    porfa deploy con reset

--- config/deploy.rb
+++ config/deploy.rb
@@ -13,8 +13,8 @@ task :seed do
   on primary fetch(:migration_role) do
     within release_path do
       with rails_env: fetch(:rails_env) do
-        execute :rake, 'db:schema:load'
-        execute :rake, 'db:seed'
+        execute :rake, "db:reset DISABLE_DATABASE_ENVIRONMENT_CHECK=1"
+        execute :rake, "db:seed DISABLE_DATABASE_ENVIRONMENT_CHECK=1"
       end
     end
   end
