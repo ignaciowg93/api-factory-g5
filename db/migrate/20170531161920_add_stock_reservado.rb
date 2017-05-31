c6a54462fa071dde1f82a6de6cfcb5f2afc76ecd
jfcumsille <jfcumsille@uc.cl>
2017-05-31 19:49:26 -0400

    fixing migrations

--- db/migrate/20170531161920_add_stock_reservado.rb
+++
@@ -1,6 +0,0 @@
-class AddStockReservado < ActiveRecord::Migration[5.0]
-  def change
-    add_column :products, :stock_reservado, :integer
-    add_column :supplies, :stock_reservado, :integer
-  end
-end
