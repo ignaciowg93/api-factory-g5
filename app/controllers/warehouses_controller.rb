3f9ad54de6c3da42c709c2153cbdbe97ebc5d272
Camila Kutscher <cakutscher@uc.cl>
2017-06-13 21:16:15 -0400

    Revert "Fix delivery method y refactoring"
    This reverts commit 6cbeb4187d1e2dd0c629fcb7968b777dfda0cc77.

--- app/controllers/warehouses_controller.rb
+++ app/controllers/warehouses_controller.rb
@@ -1,17 +1,18 @@
 class WarehousesController < ApplicationController
   before_action :get_almacenes
 
-  def stocks
-    stocks = Warehouse.get_stocks
-    render(json: stocks, status: 200)
-  end
+    def stocks
+      stocks = Warehouse.get_stocks
+      render(json: stocks, status: 200)
+    end
 
-  # Atender directamente una PO
-  def delivery
-    Warehouse.to_despacho_and_delivery(params[:id])
-  end
+    # Atender directamente una PO
+    def delivery
+      Warehouse.to_despacho_and_delivery(params[:id])
+    end
 
-  def vaciar
-    Warehouse.vaciar_almacenes
-  end
+    def vaciar
+      Warehouse.vaciar_almacenes()
+    end
+
 end
