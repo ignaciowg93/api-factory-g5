bc79629336c43e075ac797cd5741c999ea720072
Camila Kutscher <cakutscher@uc.cl>
2017-06-13 21:09:46 -0400

    Fix delivery method y refactoring

--- app/controllers/warehouses_controller.rb
+++ app/controllers/warehouses_controller.rb
@@ -1,18 +1,17 @@
 class WarehousesController < ApplicationController
   before_action :get_almacenes
 
-    def stocks
-      stocks = Warehouse.get_stocks
-      render(json: stocks, status: 200)
-    end
+  def stocks
+    stocks = Warehouse.get_stocks
+    render(json: stocks, status: 200)
+  end
 
-    # Atender directamente una PO
-    def delivery
-      Warehouse.to_despacho_and_delivery(params[:id])
-    end
+  # Atender directamente una PO
+  def delivery
+    Warehouse.to_despacho_and_delivery(params[:id])
+  end
 
-    def vaciar
-      Warehouse.vaciar_almacenes()
-    end
-
+  def vaciar
+    Warehouse.vaciar_almacenes
+  end
 end
