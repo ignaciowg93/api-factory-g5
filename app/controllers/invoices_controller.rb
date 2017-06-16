cd69454e5dad883792fe5fa82ff133c54cbd45cc
jfcumsille <jfcumsille@uc.cl>
2017-06-16 15:46:18 -0400

    fix spree problem

--- app/controllers/invoices_controller.rb
+++ app/controllers/invoices_controller.rb
@@ -209,7 +209,7 @@ class InvoicesController < ApplicationController
         response = HTTP.headers(accept: 'application/json').put(
           "#{Rails.configuration.base_route_oc}crear",
           json: {
-            cliente: temp_boleta[cliente],
+            cliente: temp_boleta["cliente"],
             proveedor: Rails.configuration.my_id,
             sku: sku,
             fechaEntrega: (Time.zone.now + 3.day).to_f * 1000,
