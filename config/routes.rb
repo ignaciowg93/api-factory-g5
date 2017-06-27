911fb4df3ba704dcccc0d729e474a17bc1885ca1
icrocco <ijcrocco@uc.cl>
2017-06-27 12:32:40 -0400

    ahora rechazamos facturas por OC con facturas ya pagadas - rutas reestablecidas

--- config/routes.rb
+++ config/routes.rb
@@ -19,32 +19,32 @@ Rails.application.routes.draw do
 #
   get 'clients' => 'clients#all'
 #Ordenes de Comprar
-  #put 'purchase_orders/:id', to: 'purchase_orders#receive'
-  #put 'purchase_orders/:id', to: 'application#receive'
+  # put 'purchase_orders/:id', to: 'purchase_orders#receive'
+  # put 'purchase_orders/:id', to: 'application#receive'
 
-#   patch 'purchase_orders(/:id(/accepted))', to: 'purchase_orders#accepted'
-#
-#   patch 'purchase_orders(/:id(/rejected))' , to: 'purchase_orders#rejected'
+  patch 'purchase_orders(/:id(/accepted))', to: 'purchase_orders#accepted'
+
+  patch 'purchase_orders(/:id(/rejected))' , to: 'purchase_orders#rejected'
 #
 # #Facturas
 #
-#   put 'invoices/:id' , to: 'invoices#receive'
-#
-#   patch 'invoices(/:id(/accepted))' , to: 'invoices#accepted'
-#
-#   patch 'invoices(/:id(/rejected))' , to: 'invoices#rejected'
-#
-#   patch 'invoices(/:id(/delivered))' , to: 'invoices#delivered'
-#
-#   patch 'invoices(/:id(/paid))' , to: 'invoices#paid'
+  put 'invoices/:id' , to: 'invoices#receive'
+
+  patch 'invoices(/:id(/accepted))' , to: 'invoices#accepted'
+
+  patch 'invoices(/:id(/rejected))' , to: 'invoices#rejected'
+
+  patch 'invoices(/:id(/delivered))' , to: 'invoices#delivered'
+
+  patch 'invoices(/:id(/paid))' , to: 'invoices#paid'
 
   put 'api/boleta' , to: 'invoices#generate_boleta'
 
 ####Lista de Precios
 
-  # get 'api/publico/precios', to: 'product#prices'
-  #
-  # get 'products', to: 'product#index'
+  get 'api/publico/precios', to: 'product#prices'
+
+  get 'products', to: 'product#index'
 
 #######
 # Metodos manuales para dashboard #
@@ -54,7 +54,7 @@ Rails.application.routes.draw do
   post 'produce', to: 'interaction#produce'
 
 # Recepcionar orden de compra
-  # put 'purchase_orders/:id', to: 'purchase_orders#receive_b2b'
+  put 'purchase_orders/:id', to: 'purchase_orders#receive_b2b'
 
 # Atender orden de compra
   post 'process/:id', to: 'purchase_orders#processPO_b2b'
