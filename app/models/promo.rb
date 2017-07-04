0bad95f9bfc6df96288f3fc0fd20667af745ba10
icrocco <ijcrocco@uc.cl>
2017-07-04 02:09:42 -0400

    debugger en twitter

--- app/models/promo.rb
+++ app/models/promo.rb
@@ -8,14 +8,6 @@ class Promo < ApplicationRecord
     validates :sku, :precio, :inicio, :fin, :codigo, presence: true
 
     def self.revisar_ofertas()
-        client = Twitter::REST::Client.new do |config|
-            config.consumer_key        = "iExeOPmt0NyO5Q5ftB0JSEuCi"
-            config.consumer_secret     = "dPfoPZpKc08CPxwIWpnK2RsGKRGwO5nN6nIMyWZxM0e1Ph5GWN"
-            config.access_token        = "879037547095220224-Smr6ICButdBqCvGBJnVua8DdzoZlHaA"
-            config.access_token_secret = "kyewGgZ9KBqrjyF8eNH7viS7P3qDkdzST0Z24FkCjkJPP"
-        end
-        Rails.logger.debug "ESTE ES EL CLIENT DE TWITTER"
-        Rails.logger.debug client
         STDOUT.sync = true
         conn = Bunny.new("#{Rails.configuration.cola_ofertas}")
         conn.start
@@ -41,6 +33,16 @@ class Promo < ApplicationRecord
                   publi = HTTP.post("https://graph.facebook.com/307193066399367/feed?message=#{to_publi}&link=#{link_fb_image}&access_token=EAADxlJnEikwBAMhlvuWmPkZAX6kWLDhZACdjf7O1QKfzHwd3UBMqZCD76yObHWGZCAhvWhGOG9hHe9Bz4nu4m8hspeCkt7I5zWmXm0IPzTmmiZAWNkpkSSLtyopmv3RjGEPk24ZCg6rD8kpO76oen3ZCkWhEj391bHXVXXvnxNvF8OcgVTtLzep")
                   to_publi_tweet = "Ahora nuestro #{product} a tan solo $#{payload["precio"]}. Aprovecha esta oferta con el codigo #{payload["codigo"]}!"
                   #publi_twitter = client.update_with_media(to_publi_tweet, File.new("app/assets/images/#{payload["sku"]}.jpg"))
+                  Rails.logger.debug "ESTE ES EL STRING DEL TWEET"
+                  Rails.logger.debug to_publi_tweet
+                  client = Twitter::REST::Client.new do |config|
+                      config.consumer_key        = "iExeOPmt0NyO5Q5ftB0JSEuCi"
+                      config.consumer_secret     = "dPfoPZpKc08CPxwIWpnK2RsGKRGwO5nN6nIMyWZxM0e1Ph5GWN"
+                      config.access_token        = "879037547095220224-Smr6ICButdBqCvGBJnVua8DdzoZlHaA"
+                      config.access_token_secret = "kyewGgZ9KBqrjyF8eNH7viS7P3qDkdzST0Z24FkCjkJPP"
+                  end
+                  Rails.logger.debug "ESTE ES EL CLIENT DE TWITTER"
+                  Rails.logger.debug client
                   Rails.logger.debug "ESTA ES LA RESPUESTA DEL TWEET"
                   publi_twitter = client.update(to_publi_tweet)
                   Rails.logger.debug publi_twitter
